705197024e0203a3ffec6be9706f5a7b749d1149
Carlos Meneses <kenshinmc23@gmail.com>
2020-03-06 15:18:50 -0400

    Add ILanguage interface

---
+++ src/main/java/org/jalasoft/moi/model/ILanguage.java
@@ -0,0 +1,17 @@
+/*
+ *   Copyright (c) 2020 Jalasoft.
+ *
+ *   This software is the confidential and proprietary information of Jalasoft.
+ *   ("Confidential Information"). You shall not disclose such Confidential
+ *   Information and shall use it only in accordance with the terms of the
+ *   license agreement you entered into with Jalasoft.
+ */
+
+package org.jalasoft.moi.model;
+
+/**
+ * This interface defines the type of object for the classes that implement it will use
+ */
+public interface ILanguage {
+    String compiler(Params params);
+}
