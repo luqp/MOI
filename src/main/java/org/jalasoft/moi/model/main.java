4f18f052d4203cad76a4fb7834ada150130b0582
Snip-77 <61165905+Snip-77@users.noreply.github.com>
2020-03-06 16:39:58 -0400

    Added Execute class that recieves the comand in sting and returns cmd response in another string

--- src/main/java/org/jalasoft/moi/model/main.java
+++ src/main/java/org/jalasoft/moi/model/main.java
@@ -11,17 +11,12 @@ package org.jalasoft.moi.model;
 
 public class main {
     public static void main(String[] arg){
-        System.out.println("Estoy en al main 1");
-        try
-        {
-            // We are running "cd", "complile" and "run" commands on cmd
-            // /K : Carries out command specified by string
-            Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"cd C:/Users/MauricioOroza/com && javac *.java && java MainClass\"");
-        }
-        catch (Exception e)
-        {
-            System.out.println("HEY Buddy! U r Doing Something Wrong ");
-            e.printStackTrace();
-        }
+        String comm1="cmd.exe /c \"cd C:/Users/MauricioOroza/com && javac *.java && java MainClass\"";
+        String comm2="help";
+        String output;
+
+        Execute buildedcomm=new Execute(comm1);
+        output=buildedcomm.run();
+        System.out.println(output);
     }
 }
