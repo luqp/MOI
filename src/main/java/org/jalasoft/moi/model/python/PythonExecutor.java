4006cc202ef95a3face96c8002cd22e568e586cc
luqp <luceroqp@outlook.com>
2020-03-06 10:59:09 -0400

    Apply standards.

--- src/main/java/org/jalasoft/moi/model/python/PythonExecutor.java
+++ src/main/java/org/jalasoft/moi/model/python/PythonExecutor.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2020 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Jalasoft.
+ */
+
 package org.jalasoft.moi.model.python;
 
 import java.io.BufferedReader;
@@ -33,8 +42,9 @@ public class PythonExecutor {
             BufferedReader br = new BufferedReader(input);
             String line = null;
             System.out.println("<OUTPUT>");
-            while ((line = br.readLine()) != null)
+            while ((line = br.readLine()) != null) {
                 System.out.println(line);
+            }
             System.out.println("</OUTPUT>");
             System.out.println(process.waitFor());
         } catch (IOException | InterruptedException e) {
