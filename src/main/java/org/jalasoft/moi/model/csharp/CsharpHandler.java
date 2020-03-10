626b3f22454743fd15dd04ff7383620c5c2df159
Carlos Meneses <kenshinmc23@gmail.com>
2020-03-09 22:49:23 -0400

    Implement IHandler interface and CsharpHandler class

---
+++ src/main/java/org/jalasoft/moi/model/csharp/CsharpHandler.java
@@ -0,0 +1,44 @@
+/*
+ *   Copyright (c) 2020 Jalasoft.
+ *
+ *   This software is the confidential and proprietary information of Jalasoft.
+ *   ("Confidential Information"). You shall not disclose such Confidential
+ *   Information and shall use it only in accordance with the terms of the
+ *   license agreement you entered into with Jalasoft.
+ */
+
+package org.jalasoft.moi.model.csharp;
+
+import org.jalasoft.moi.model.core.Executer;
+import org.jalasoft.moi.model.core.IHandler;
+import org.jalasoft.moi.model.core.Params;
+import java.io.IOException;
+
+/**
+ * Handler is in charge to work with the executer and the command builder
+ *
+ * @version 1.0
+ * @author Carlos Meneses
+ */
+public class CsharpHandler implements IHandler {
+
+    /**
+     * @param params contains the parameters to build a command a execute it
+     * @return a String of result from CommandBuilder and Executer handling
+     */
+    @Override
+    public String execute(Params params) {
+        CsharpCommandBuilder cSharpCommandBuilder = new CsharpCommandBuilder();
+        String command = cSharpCommandBuilder.buildCommand(params.getFilesPath());
+
+        Executer executer = new Executer(command);
+        String result;
+        try {
+            result = executer.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+            result = "Nothing for compile";
+        }
+        return result;
+    }
+}
