ee1221084a062f11eb7dfb23cdb99997d14a09f3
Carlos Meneses <kenshinmc23@gmail.com>
2020-03-13 11:43:32 -0400

    add documentation about swagger

--- src/main/java/org/jalasoft/moi/config/SwaggerConfig.java
+++ src/main/java/org/jalasoft/moi/config/SwaggerConfig.java
@@ -1,3 +1,12 @@
+/*
+ *  Copyright (c) 2020 Jalasoft.
+ *
+ *  This software is the confidential and proprietary information of Jalasoft.
+ *  ("Confidential Information"). You shall not disclose such Confidential
+ *  Information and shall use it only in accordance with the terms of the
+ *  license agreement you entered into with Jalasoft.
+ */
+
 package org.jalasoft.moi.config;
 
 import org.springframework.context.annotation.Bean;
@@ -9,9 +18,21 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * This class implement basic configurations to get Swagger running in our project
+ * and use it for specifications and tools that help us document our APIs
+ *
+ * @author Carlos Meneses
+ * @version 1.1
+ */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+
+    /**
+     * @return The documentation that swagger shows on the browser
+     */
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -24,6 +45,9 @@ public class SwaggerConfig {
 
     }
 
+    /**
+     * @return Information that we are going to show on the browser about the rest API
+     */
     private ApiInfo apiInfo() {
         return new ApiInfo("Multilaguage Online Ide",
                 "This api is a service of onLine Ide",
