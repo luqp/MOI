a6cdb37108b10d80a37f25db5e33a7c0d2b656ca
Luqp <luceroqp@outlook.com>
2020-03-25 08:42:19 -0400

    Clean code.

--- src/main/java/org/jalasoft/moi/model/python/PythonCommandBuilder.java
+++ src/main/java/org/jalasoft/moi/model/python/PythonCommandBuilder.java
@@ -1,9 +1,17 @@
+/*
+ * Copyright (c) 2020 Jalasoft.
+ *
+ * This software is the confidential and proprietary information of Jalasoft.
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Jalasoft.
+ */
+
 package org.jalasoft.moi.model.python;
 
 import org.jalasoft.moi.model.core.ICommandBuilder;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
  * Builds commands to execute a python file.
--- src/test/java/org/jalasoft/moi/model/python/PythonCommandBuilderTest.java
+++ src/test/java/org/jalasoft/moi/model/python/PythonCommandBuilderTest.java
@@ -9,16 +9,13 @@
 
 package org.jalasoft.moi.model.python;
 
-
-import org.jalasoft.moi.model.core.*;
+import org.jalasoft.moi.model.core.ICommandBuilder;
+import org.jalasoft.moi.model.core.Language;
 import org.jalasoft.moi.model.core.parameters.Params;
 import org.jalasoft.moi.model.utils.Constant;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import java.io.IOException;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -49,31 +46,5 @@ public class PythonCommandBuilderTest {
         String commandResult = pythonCommandBuilder.buildCommand(params.getFilesPath());
 
         assertEquals(command, commandResult);
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-            "'Hello World\n', test1.py",
-            "'Hello World\nHello Team\nHello you\n', test2.py"
-    })
-    public void executeFileTest(String expect, String file) {
-//        Params params = new Params();
-//        params.setFilesPath(Paths.get(directoryFile + file));
-//        params.setLanguage(Language.PYTHON_32);
-//        ICommandBuilder pythonCommandBuilder = params.getLanguage().getCommandBuilder();
-//
-//        String commandResult = pythonCommandBuilder.buildCommand(params.getFilesPath());
-//        Executer executer = new Executer(commandResult);
-//        Result result;
-//        try {
-//            result = executer.execute();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            result = new Result();
-//            result.setPid(0);
-//            result.setResult(e.getMessage());
-//        }
-//
-//        assertEquals(expect, result.getResult());
     }
 }
